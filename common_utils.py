bcfcab950978a571e7cd400ada96560e0b528611
jhj9109 <jhj91_09@naver.com>
2023-06-12 22:26:10 +0900

    Add common_utils.py
    공통으로 적용해야할 함수 & 로직과 독립적인 유틸성 함수 작성
    get_worksheet_name()
    - 워크시트명으로 활용될 문자열을 만들어내는 함수

---
+++ common_utils.py
@@ -0,0 +1,32 @@
+def get_worksheet_name(단지명, 몇동):
+    return f"{단지명}-{몇동}동"
+
+def sorted_files(depth, folder_path):
+    import os
+    
+    files = []
+    
+    for entry in os.scandir(folder_path):
+        if entry.is_file():
+            # 파일인 경우 파일 목록에 추가
+            files.append(entry.name)
+        elif entry.is_dir() and depth > 0:
+            # 폴더인 경우 && depth가 남아있다면, 재귀 호출을 통해 파일 목록 추가
+            sorted_subfolder_files = sorted_files(depth - 1, entry.path)
+            files.extend(sorted_subfolder_files)
+
+    files.sort() # 정렬
+
+    return files
+
+def get_config_from_json(filepath):
+    import json
+    try:
+        with open(filepath, 'r') as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        raise Exception(f"파일 '{filepath}'을(를) 찾을 수 없습니다.")
+    except json.JSONDecodeError:
+        raise Exception(f"파일 '{filepath}'의 JSON 형식이 올바르지 않습니다.")
+
+    return config
