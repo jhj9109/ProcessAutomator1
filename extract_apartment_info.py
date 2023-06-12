5118ce79957bf811fd47eccd4fef649746e30fc8
jhj9109 <jhj91_09@naver.com>
2023-06-12 22:30:01 +0900

    UPDATE 코드 개선 및 대상세대수 구하는 로직 추가
    대상세대수 구하기
    - 워크시트에서 가장 큰 index값이 대상세대수를 의미
    - 시작 당시 공실인 세대의 경우, index가 없으므로 적절히 핸들링함(무시)

--- extract_apartment_info.py
+++ extract_apartment_info.py
@@ -1,71 +1,82 @@
-import openpyxl
+from openpyxl import load_workbook
 import json
 from collections import defaultdict
 
-def analyze_apartments(추출할엑셀파일경로, 설정파일명, 엑셀파일명):
-    # 엑셀 파일 열기
-    workbook = openpyxl.load_workbook(추출할엑셀파일경로)
+def is_dong_ho(dong, ho):
+    return isinstance(dong, int) and isinstance(ho, int)
 
-    # 분석 결과를 저장할 JSON 객체 생성
-    config = {
-        '아파트목록': []
+def process_index_dong_ho(apartment_data, max_index, index, dong, ho):
+    if is_dong_ho(dong, ho):
+        apartment_data[dong].append(ho)
+        if isinstance(index, int):
+            max_index = max(max_index, index)
+    return max_index
+
+def get_apartment_info(sheetname):
+    
+    # 시트 열기
+    ws = workbook[sheetname]
+    
+    # 시트별로 동호수를 저장할 리스트 생성
+    apartment_data = defaultdict(list)
+    max_index = 0
+
+    # 시트의 모든 행 순회
+    for row in ws.iter_rows(min_row=3):
+        # 동호수가 있는 열의 값을 조회 + 순번(처음부터 빈 집은 순번이 비어있을 수 있다! => 빼고 카운팅 됨)
+        max_index = process_index_dong_ho(apartment_data, max_index, row[0].value, row[1].value, row[2].value)
+        max_index = process_index_dong_ho(apartment_data, max_index, row[5].value, row[6].value, row[7].value)
+
+    # 정렬하기
+    for k, v in apartment_data.items():
+        apartment_data[k] = sorted(v)
+    
+    apartment_info = {
+        '단지명': sheetname,
+        '동호수목록': apartment_data,
+        # '동호수목록2': new_data,
+        '대상세대수': max_index
     }
 
-    # 각 시트 순회
-    for sheet_name in workbook.sheetnames:
-        # 시트 열기
-        sheet = workbook[sheet_name]
+    return apartment_info
 
-        # 시트별로 동호수를 저장할 리스트 생성
-        apartment_data = defaultdict(list)
+def get_new_data(apartment_data):
+    result = dict()
+    for k, v in apartment_data.items():
+        lst = []
+        i = 0
+        while i < len(v):
+            j = 1
+            while i + j < len(v) and v[i]+j == v[i+j]:
+                j += 1
+            lst.append([v[i], v[i] + j - 1])
+            i += j 
+        result[k] = lst
+    return result
 
-        # 시트의 모든 행 순회
-        for row in sheet.iter_rows(min_row=3):
-            # 동호수가 있는 열의 값을 조회
-            dong = row[1].value
-            ho = row[2].value
-            dong2 = row[6].value
-            ho2 = row[7].value
+def analyze_apartments(추출할엑셀파일경로, 설정파일명, 엑셀파일명):
 
-            # 유효한 값인지 확인 (둘 다 숫자여야 함)
-            if isinstance(dong, int) and isinstance(ho, int):
-                apartment_data[dong].append(ho)
+    # 엑셀 파일 열기
+    workbook = load_workbook(추출할엑셀파일경로)
 
-            if isinstance(dong2, int) and isinstance(ho2, int):
-                apartment_data[dong2].append(ho2)
-
-        for k, v in apartment_data.items():
-            apartment_data[k] = sorted(v)
-
-        new_data = dict()
-        for k, v in apartment_data.items():
-            lst = []
-            i = 0
-            while i < len(v):
-                j = 1
-                while i + j < len(v) and v[i]+j == v[i+j]:
-                    j += 1
-                lst.append([v[i], v[i] + j - 1])
-                i += j 
-            new_data[k] = lst
-        # 분석 결과를 JSON에 추가
-        apartment = {
-            '단지명': sheet_name,
-            '동호수목록': apartment_data,
-            '동호수목록2': new_data
-        }
-        config['아파트목록'].append(apartment)
+    # 각 시트를 순회하여 아파트목록 데이터 생성
+    아파트목록 = [get_apartment_info(sheetname) for sheetname in workbook.sheetnames]
 
     # JSON 파일로 저장
-    config['설정파일명'] = 설정파일명
-    config['엑셀파일명'] = 엑셀파일명
+    config = {
+        '아파트목록': 아파트목록
+        '설정파일명': 설정파일명
+        '엑셀파일명': 엑셀파일명
+    }
+
     with open(설정파일명, 'w', encoding='utf-8') as json_file:
         json.dump(config, json_file, ensure_ascii=False, indent=4)
 
-# 엑셀 파일 경로
-추출할엑셀파일경로 = 'a.xlsx'
-설정파일명 = 'apart.json'
-엑셀파일명 = 'summary.xlsx'
+if __name__ == '__main__':
+    
+    추출할엑셀파일경로 = '00 - 입주자서명부.xlsx'
+    설정파일명 = 'apartments.json'
+    엑셀파일명 = 'summary.xlsx'
 
-# 아파트 분석 실행
-analyze_apartments(추출할엑셀파일경로, 설정파일명, 엑셀파일명)
+    # 아파트 분석 실행
+    analyze_apartments(추출할엑셀파일경로, 설정파일명, 엑셀파일명)
