4a24d948eb43df90eafeab1ac90d7f34548a973a
jhj9109 <jhj91_09@naver.com>
2023-07-07 14:49:37 +0900

    UPDATE ws._images를 가공하는 코드 추가
    확실치 않지만, ws._images에 저장된 이미지는 적은 정보를 갖는다
    - _data: 이미지를 표현할 데이터
    - anchor: 어느 쉘에 구속되는지?
      - anchor._from : 0부터 시작되는 row, col 정보 갖음
    
    추가당시 이미지 이름을 참고가능한지 찾아봤지만 실패
    row, col을 이용해 이미지를 identify하고 있음

--- image_utils.py
+++ image_utils.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 ORIENTATION_TAG = 0x0112
 
 ori_kr = {
@@ -13,6 +15,15 @@ ori_to_degree = {
     6: -90,
     8: 90,
 }
+
+''' constant values
+현재 구조에서 이미지는 3번째 행부터 홀수 행에서 1, 3열에 위치한다.
+- (행 - 첫번째아이템행)이 짝수여야 한다.
+- 열은 1, 3열이여야 한다.
+'''
+현관사진_COLUMN = 1
+큐알사진_COLUMN = 3
+FIRST_ITEM_ROW_INDEX = 3
 
 def get_orientation(pil_image):
     try:
@@ -37,6 +48,57 @@ def print_image_info(image_path):
 
         print(f"이미지 회전: {orientation} ({ori_kr[orientation]})")
 
+def is_valid_anchor(row, col):
+    return row >= FIRST_ITEM_ROW_INDEX and \
+        (row - FIRST_ITEM_ROW_INDEX) % 2 == 0 and \
+        (col != 현관사진_COLUMN and col != 큐알사진_COLUMN)
+
+def is_pair(images_in_row):
+    return 현관사진_COLUMN in images_in_row and 큐알사진_COLUMN in images_in_row
+
+def get_image_records(ws, raise_exception_flag = True):
+ 
+    image_records = defaultdict(dict)
+
+    try:
+            
+        
+        for index_in__image, img in enumerate(ws._images):
+            '''
+            ws.cell(row, col) 과정에서 row와 col은 1부터 시작하는값.
+            img.anchor._from 의 row, col은 0부터 시작하는 값.
+            '''
+            ROW_OFFSET = 1 
+            COLUMN_OFFSET = 1
+                
+            row = img.anchor._from.row + ROW_OFFSET
+            col = img.anchor._from.col + COLUMN_OFFSET
+
+            if not is_valid_anchor(row, col): # 이미지가 위치가능한 좌표가 있음
+                raise Exception(f"Invalid image anchor's (row, col): ({row, col})")
+            
+            if col in image_records[row]: # 하나의 셀에 여러 이미지 앵커가 가르킴
+                raise Exception(f"Duplicated image anchor at ({row, col}): {image_records[row][col], index_in__image}")
+
+            image_records[row][col] = index_in__image
+            
+                
+        for row, images_in_row in image_records.items():
+            if not is_pair(images_in_row):
+                raise Exception(f"짝이 안맞음 row, images_in_row: {row, images_in_row}")
+
+    except Exception as error:
+
+        if raise_exception_flag:
+            raise(error)
+        print(f"get_image_records()에서 Exception 발생")
+        print(error)
+
+    return image_records
+
+
+def is_mpo(image):
+    return image.format.upper() == 'MPO'
 # def reset_rotation_info(image):
 #     # 이미지의 exif 정보 가져오기
 #     print(image)
