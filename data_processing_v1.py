c7a91c951adcf9ba71d31d95f46598b696e96669
jhj9109 <jhj91_09@naver.com>
2023-07-07 16:16:40 +0900

    UPDATE 출력파일분리, 업데이트 적용, 카운팅로직
    data_processing코드에서 출력파일 분리 적용
    기존 엑셀파일의 이미지를 활용하여 신규, 업데이트 분리
    카운팅및 로그 json파일로 덤프하는 코드 적용

--- data_processing_v1.py
+++ data_processing_v1.py
@@ -2,23 +2,28 @@
 from openpyxl import Workbook, load_workbook
 from openpyxl.drawing.image import Image
 from openpyxl.styles import Font, Alignment
-#from PIL import Image as PILImage # 이미지 크기를 줄이고 싶을때, 이것을 활용해야할듯.
+# from PIL import Image as PILImage # 이미지 크기를 줄이고 싶을때, 이것을 활용해야할듯.
 
-import json # json 파일에서 설정을 사용하고자함
-import sys # 파일 실행시 인자 받아서 활용하기
-import os # 해당경로 하위 파일 파악하는데 활용
-import re # 파일명에서 정보 추출하는데 활용
+import json  # json 파일에서 설정을 사용하고자함
+import sys  # 파일 실행시 인자 받아서 활용하기
+import os  # 해당경로 하위 파일 파악하는데 활용
+import re  # 파일명에서 정보 추출하는데 활용
 
 from collections import defaultdict
 
 # 두개이상의 파일에서 공통으로 가져가야할 규칙은 import해서 사용하기
 from common_utils import get_worksheet_name, get_xlsx_file_name, sorted_file_entries, load_json, get_apart_object
 from image_preprocessing import handle_rotated_or_mpo_image
+from image_utils import get_image_records
+
+import pdb
+
+from colorama import Fore, Back, Style
 
 DEBUG_MODE = False
 
 PX_TO_PT = 3 / 4
-PT_TO_PX = 4 / 3 # 1.33
+PT_TO_PX = 4 / 3  # 1.33
 
 IMAGE_CELL_HEIGHT_PT = 160
 IMAGE_CELL_HEIGHT_PX = IMAGE_CELL_HEIGHT_PT * PT_TO_PX
@@ -40,26 +45,38 @@ FIRST_ITEM_ROW_INDEX = 3
 ENUM_현관사진 = 0
 ENUM_큐알사진 = 1
 
-def insert_image_with_cell_height(ws, image_path, cell, 현관사진여부 = False):
+신규_플래그 = 0
+업데이트_플래그 = 1
 
-    save_rotated_image(image_path)
-    
+
+def insert_image_with_cell_height(ws, image_path, cell, 현관사진여부, 기존이미지인덱스=-1):
+
+    handle_rotated_or_mpo_image(image_path)
+
     img = Image(image_path)
 
     # 이미지 사이즈 조절 => 원본 크기를 조절하는것은 아니였던듯? 하려면 pillow의 Image 직접 사용해야하는듯
     img.width = IMAGE_CELL_HEIGHT_PX * img.width / img.height
     img.height = IMAGE_CELL_HEIGHT_PX
 
-    if 현관사진여부 and img.width > img.height :
+    if 현관사진여부 and img.width > img.height:
         img.height = img.height * IMAGE_CELL_WIDTH_PX / img.width
         img.width = IMAGE_CELL_WIDTH_PX
 
-    # 이미지 삽입
-    ws.add_image(img, cell.coordinate)
+    # 이미지 삽입 or 교체
+    if 기존이미지인덱스 != -1:
+        # print(f"{기존이미지인덱스}: 이미지교체")
+        img.anchor = cell.coordinate
+        ws._images[기존이미지인덱스] = img
+    else:
+        # print(f"{기존이미지인덱스}: 이미지추가")
+        ws.add_image(img, cell.coordinate)
+
 
 def check_extension(filename, extname):
     pattern = rf"\.{extname}$"
     return bool(re.search(pattern, filename))
+
 
 def extract_extension(filename):
     pattern = r"\.([^.]+)$"
@@ -68,6 +85,7 @@ def extract_extension(filename):
         return match.group(1)
     else:
         return None
+
 
 def extract_filename_components(filename):
     pattern = r"^(.+)\.([^.]+)$"
@@ -78,6 +96,7 @@ def extract_filename_components(filename):
     else:
         return "", "", ""
 
+
 def 동호수_유효성체크(동호수목록, 동, 호수):
     try:
         return 동호수목록[동].index(int(호수))
@@ -86,101 +105,202 @@ def 동호수_유효성체크(동호수목록, 동, 호수):
     except ValueError:
         return -1
 
-def 워크시트하나에_이미지_한개_삽입하기(ws, 현관사진_파일경로, 큐알사진_파일경로, 인덱스):
-    
-    
-    row_index = FIRST_ITEM_ROW_INDEX + 인덱스 * 2
-    
+
+def get_row_index(index):
+    return FIRST_ITEM_ROW_INDEX + index * 2
+
+
+def 워크시트하나에_이미지_한개_삽입하기(ws, r, 현관사진_파일경로, 큐알사진_파일경로, 인덱스):
+
+    result = 신규_플래그
+
+    현관사진_COLUMN = 1
+    큐알사진_COLUMN = 3
+
+    row_index = get_row_index(인덱스)
+
     현관사진_cell = ws.cell(row=row_index, column=1)
     큐알사진_cell = ws.cell(row=row_index, column=3)
-    insert_image_with_cell_height(ws, 현관사진_파일경로, 현관사진_cell, True)
-    insert_image_with_cell_height(ws, 큐알사진_파일경로, 큐알사진_cell)
+    
+    index = [-1, -1]
+
+    # pdb.set_trace()
+    
+    if row_index in r:
+        # print("업데이트")
+        index = [r[row_index][현관사진_COLUMN], r[row_index][큐알사진_COLUMN]]
+        result = 업데이트_플래그
+
+    insert_image_with_cell_height(ws, 현관사진_파일경로, 현관사진_cell, True, index[0])
+    insert_image_with_cell_height(ws, 큐알사진_파일경로, 큐알사진_cell, False, index[1])
+
+    return result
+
+def 동_호수_큐알사진여부_패턴매칭하기(filename):
+    # pattern1 = r'^(\d+)동\s*(\d+)호\s*(\(1\))?(\(2\))?\s*\.(?:png|jpg|jpeg|JPEG|PNG|JPG)$'
+    # pattern2 = r'^(\d+)동\s*(\d+)호\s*(\(1\))?(\(2\))?\s*\.(?:png|jpg|jpeg|JPEG|PNG|JPG)$'
+    # pattern3 = r'^(\d+)[^\d]+(\d+)[^\d]+(\(1\))?(\(2\))?\s*\.(?:png|jpg|jpeg|JPEG|PNG|JPG)$'
+    # pattern4 = r'^(\d+)[^\d\(\)]+(\d+)[^\d\(\)]+(\(1\))?(\(2\))?\s*\.(?:png|jpg|jpeg|JPEG|PNG|JPG)$'
+    pattern5 = r'^(\d+)[^\d\(\)]+(\d+)[^\d\(\)]+\(([12])\)\s*\.(?:png|jpg|jpeg|JPEG|PNG|JPG)$' # (1), (2) 반드시
+    pattern6 = r'^(\d+)-(\d+)(_1)?\.(?:png|jpg|jpeg|JPEG|PNG|JPG)$'
+
+    # 111-444.jpg => 현관사진, 111-444_1.jpg => 큐알사진
+    matched = re.match(pattern6, filename)
+    if matched:
+        동, 호수, 큐알사진여부 = matched.groups(False)
+        큐알사진여부 = bool(큐알사진여부)
+        # print(f"{Fore.RED}{filename}{Style.RESET_ALL}")
+        return (동, 호수, 큐알사진여부)
+    
+    matched = re.match(pattern5, filename)
+    if matched:
+        동, 호수, 큐알사진여부 = matched.groups(False)
+        큐알사진여부 = 큐알사진여부 == '2'
+        # print(f"{Fore.BLUE}{filename}{Style.RESET_ALL}")
+        return (동, 호수, 큐알사진여부)
+
+    # print(f"{Fore.YELLOW}{filename}{Style.RESET_ALL}")
+    return None
 
 
-def 작업분_기존엑셀에_반영하기(wb, 단지명, 동호수목록, file_entries):
+def 동별_작업분_생성하기(동호수목록, file_entries):
     # 동별로 하나의 시트 => 동별로 구분 짓기.
-    동별_작업분 = { int(동): defaultdict(lambda: ['', '', -1]) for 동 in 동호수목록.keys() }
+    동별_작업분 = {int(동): defaultdict(lambda: ['', '', -1]) for 동 in 동호수목록.keys()}
     ''' 예시
     동별_작업분[101][777] = [현관사진경로, 큐알사진경로, 워크시트명, 호수 - 시작호수]
     - 워크시트명과 호수-시작호수는 필요 없어진것일수도 있지만.... 일단 냅두자
     '''
-    
-    성공적으로업데이트, 파일명이_매칭이_안되어_실패, 유효성체크_실패, 사진_하나라도_없어_실패 = [], [], [], []
-    
+
+    파일명이_매칭이_안되어_실패, 유효성체크_실패 = [], []
+
     for entry in file_entries:
         filename = entry.name
-        pattern1 = r'^(\d+)동\s*(\d+)호\s*(\(1\))?(\(2\))?\s*\.(?:png|jpg|jpeg)$' # 동호를 붙여넣기
-        pattern2 = r'^(\d+)동\s*(\d+)호\s*(\(1\))?(\(2\))?\s*\.(?:png|jpg|jpeg)$'
-        matched = re.match(pattern1, filename) or re.match(pattern2, filename)
-
-        if not matched:
+        
+        result = 동_호수_큐알사진여부_패턴매칭하기(filename)
+        
+        if result is None:
             파일명이_매칭이_안되어_실패.append(filename)
             continue
-    
-        동, 호수, _현관사진여부, 큐알사진여부 = matched.groups(False)
+        
+        동, 호수, 큐알사진여부 = result
         인덱스 = 동호수_유효성체크(동호수목록, 동, 호수)
 
         if 인덱스 == -1:
             유효성체크_실패.append(filename)
             continue
-    
+
         동, 호수 = map(int, [동, 호수])
         사진모드 = ENUM_큐알사진 if 큐알사진여부 else ENUM_현관사진
 
         동별_작업분[동][호수][사진모드] = entry.path
         동별_작업분[동][호수][2] = 인덱스
-        
+
     # # 디버깅중
     # for i, lst in enumerate([파일명이_매칭이_안되어_실패, 유효성체크_실패]):
     #     print(["파일명이_매칭이_안되어_실패", "유효성체크_실패"][i])
     #     print(lst)
 
-    for 동, 호별작업물 in 동별_작업분.items():
-        
-        워크시트명 = get_worksheet_name(단지명, 동)
-        ws = wb[워크시트명]
-        
-        for 현관사진_파일경로, 큐알사진_파일경로, 인덱스 in 호별작업물.values():
-            
-            if 현관사진_파일경로 != '' and 큐알사진_파일경로 != '':
-                워크시트하나에_이미지_한개_삽입하기(ws, 현관사진_파일경로, 큐알사진_파일경로, 인덱스)
-                성공적으로업데이트.append((현관사진_파일경로, 큐알사진_파일경로, 인덱스))
-            else:
-                사진_하나라도_없어_실패.append((현관사진_파일경로, 큐알사진_파일경로, 인덱스))
+    return 동별_작업분, 파일명이_매칭이_안되어_실패, 유효성체크_실패
+
+
+def 작업분_기존엑셀에_반영하기(base_path, 출력파일정보객체, 단지명, 동호수목록, file_entries):
+
+    동별세이브_플래그 = False
+    호별세이브_플래그 = False
+
+    동별_작업분, 파일명이_매칭이_안되어_실패, 유효성체크_실패 = 동별_작업분_생성하기(동호수목록, file_entries)
+    성공적으로업데이트, 사진_하나라도_없어_실패 = [], []
+    작업량 = {"기존": 0, "신규": [], "업데이트": []}  # 신규, 업데이트
+
+    for 출력파일명, 동목록 in 출력파일정보객체.items():  # "서울중계9_1", [901, 902, 903, 904, 905]
+
+        파일경로 = os.path.join(base_path, get_xlsx_file_name(출력파일명))
+        wb = load_workbook(filename=파일경로)
+
+        for 동 in 동목록:
+
+            워크시트명 = get_worksheet_name(단지명, 동)
+            ws = wb[워크시트명]
+            r = get_image_records(ws)
+            # print(r)
+            작업량["기존"] += len(r)
+
+            호별작업물 = 동별_작업분[동]
+
+            for 현관사진_파일경로, 큐알사진_파일경로, 인덱스 in 호별작업물.values():
+
+                if 현관사진_파일경로 != '' and 큐알사진_파일경로 != '':
+
+                    result = 워크시트하나에_이미지_한개_삽입하기(ws, r, 현관사진_파일경로, 큐알사진_파일경로, 인덱스)
+                    if result == 신규_플래그:
+                        작업량["신규"].append({
+                            "현관사진_파일경로": 현관사진_파일경로,
+                            "큐알사진_파일경로": 큐알사진_파일경로,
+                            "인덱스": 인덱스
+                        })
+                    else:
+                        작업량["업데이트"].append({
+                            "현관사진_파일경로": 현관사진_파일경로,
+                            "큐알사진_파일경로": 큐알사진_파일경로,
+                            "인덱스": 인덱스
+                        })  
+                    성공적으로업데이트.append((현관사진_파일경로, 큐알사진_파일경로, 인덱스))
+                else:
+                    사진_하나라도_없어_실패.append((현관사진_파일경로, 큐알사진_파일경로, 인덱스))
+
+                if 호별세이브_플래그:
+                    wb.save(파일경로)
+                    wb.close()
+                    print(f"호별 세이브: {현관사진_파일경로}, {큐알사진_파일경로} 완료")
+                    wb = load_workbook(filename=파일경로)
+
+            if not 호별세이브_플래그 and 동별세이브_플래그:
+                wb.save(파일경로)
+                wb.close()
+                print(f"{단지명} {동}동: 동별 세이브 완료")
+                wb = load_workbook(filename=파일경로)
+
+        if not 호별세이브_플래그 and not 동별세이브_플래그:
+            wb.save(파일경로)
+            wb.close()
+            print(f"{출력파일명}: 파일별 세이브 완료")
+    결과 = {
+        "작업량": 작업량,
+        # "성공적으로업데이트": 성공적으로업데이트, # 작업량과 중복
+        "사진_하나라도_없어_실패": 사진_하나라도_없어_실패,
+        "파일명이_매칭이_안되어_실패": 파일명이_매칭이_안되어_실패,
+        "유효성체크_실패": 유효성체크_실패
+    }
+    return 결과
     # # 디버깅중
     # for i, lst in enumerate([성공적으로업데이트, 사진_하나라도_없어_실패]):
     #     print(["성공적으로업데이트", "사진_하나라도_없어_실패"][i])
     #     print(lst)
 
+
 DEFAULT_CONFIG_FILE_PATH = "./apartments.json"
 
+
 def update_one_apartment(config, folder_path, 단지명, base_path):
-    
+
     # 1. 유효한 아파트 단지인지 체크
     아파트객체 = get_apart_object(config, 단지명)
-    
+
     # 2. 폴더 하위 모든 파일 추출 => path까지 가진 DirEntry로 변경
     file_entries = sorted_file_entries(-1, folder_path)
-    
+
     # 2-1. 하나도 파일이 없으면 에러
     if not file_entries:
         raise Exception(f"{folder_path}디렉토리 아래에 파일이 존재하지 않습니다.")
 
-    # 3. 아파트단지 하나에 대한 엑셀파일(워크북)을 연다.
-    
-    파일경로 = os.path.join(base_path, get_xlsx_file_name(단지명))
-    wb = load_workbook(filename = 파일경로)
-    
-    # 4. 아파트객체에서 동호수목록을 가지고 유효성체크하며 기존 엑셀에 반영
+    # 3. 아파트객체에서 동호수목록을 가지고 유효성체크하며 기존 엑셀에 반영
     동호수목록 = 아파트객체["동호수목록"]
-    작업분_기존엑셀에_반영하기(wb, 단지명, 동호수목록, file_entries)
-    
-    # 5. 작업 완료후 아래 코드로 저장하여 반영하기
-    wb.save(파일경로)
-    # wb.save(os.path.join(base_path, "sample.xlsx"))
-    
-    # 6. 파일 닫기
-    wb.close()
+
+    # 4. 엑셀 파일 열기 위한 정보. json파일로 미리 만들어 놓던지 한다.
+    출력파일정보객체 = 아파트객체["출력파일정보객체"]
+    결과객체 = 작업분_기존엑셀에_반영하기(base_path, 출력파일정보객체, 단지명, 동호수목록, file_entries)
+    return 결과객체
+
 
 '''
 1. 먼저 설정파일을 읽어서 아파트목록 데이터를 취한다.
@@ -190,12 +310,12 @@ def update_one_apartment(config, folder_path, 단지명, base_path):
 5. 업데이트된 엑셀파일을 저장한다.
 '''
 if __name__ == '__main__':
-    
+
     config = load_json(DEFAULT_CONFIG_FILE_PATH)
-    
+
     # 0. 커맨드라인 인수로부터 정보 입력 받음
     folder_path = sys.argv[1]
     단지명 = sys.argv[2]
     base_path = "./" if len(sys.argv) < 4 else sys.argv[3]
-    
+
     update_one_apartment(config, folder_path, 단지명, base_path)
--- update_in_folders.py
+++ update_in_folders.py
@@ -1,42 +1,120 @@
-import sys # for 실행인자
-import os # for 파일시스템 조회
+import sys  # for 실행인자
+import os  # for 파일시스템 조회
 
 from common_utils import get_xlsx_file_name, sorted_file_entries, load_json, get_apart_object, get_apart_object2
 
 from data_processing_v1 import update_one_apartment
 
-DEFAULT_CONFIG_FILE_PATH = "./apartments.json"
+from counting import 카운팅_업데이트
 
-if __name__ == '__main__':
-    
-    config = load_json(DEFAULT_CONFIG_FILE_PATH)\
-    
-    # 0. 커맨드라인 인수로부터 정보 입력 받음
-    folder_path = sys.argv[1]
-    base_path = "." if len(sys.argv) < 3 else sys.argv[2]
-    
+import json
+from datetime import date
+
+import traceback
+
+from colorama import Fore, Back, Style
+
+def get_entries(folder_path):
+
     pass_files = []
     folder_entries = []
-    for entry in os.scandir(folder_path):
-        if entry.is_file():
-            # 폴더만 순회할 것임. 파일은 기록용으로 출력만
-            pass_files.append(entry.name)
-        else:
-            folder_entries.append(entry)
     
+    for entry in os.scandir(folder_path):
+        
+        if entry.is_file(): # 폴더만 순회할 것임. 파일은 기록용으로 출력만
+            
+            pass_files.append(entry.name)
+
+        else:
+
+            folder_entries.append(entry)
+
+    return pass_files, folder_entries
+
+def update_all_apartment(config, base_path, folder_entries):
+    # 서머리용
+    결과객체모음 = dict()
+
     # 각 폴더는 아파트 단지 하나 => 단지명
-    for entry in folder_entries:
+    print("단지별 업데이트 시작")
+    for i, entry in enumerate(folder_entries, start=1):
         try:
-            순번 = int(entry.name[:2])
-            # 단지명 = name[2:] # 앞에 두자리 제외 => 01서울번동3 => 서울번동3
             '''
             folder_path => 해당 폴더 아래의 모든 파일을 DirEntry로 추출할때 사용하기 위함.
             단지명 => 아파트 객체 & 해당 아파트 엑셀 파일 경로
             base_path => 해당 아파트 엑셀 파일 경로
             '''
+            # 단지명 = name[2:] # 앞에 두자리 제외 => 01서울번동3 => 서울번동3
+            순번 = int(entry.name[:2])
+
             아파트객체 = get_apart_object2(config, 순번)
-            print(f"{아파트객체['단지명']}단지 {아파트객체['대상세대수']}세대에 대한 작업 시작")
-            update_one_apartment(config, entry.path, 아파트객체["단지명"], base_path)
-        except:
+
+            print(
+                f"{Fore.RED}({i}/{len(folder_entries)}){아파트객체['단지명']}단지 {아파트객체['대상세대수']}세대에 대한 작업 시작{Style.RESET_ALL}")
+
+            결과객체 = update_one_apartment(
+                config, entry.path, 아파트객체["단지명"], base_path)
+
+            결과객체모음[아파트객체["단지명"]] = 결과객체
+
+            print(f"{Fore.BLUE}({i}/{len(folder_entries)}) 작업 종료{Style.RESET_ALL}")
+
+        except Exception as error:
+
             print(f"{entry.name}폴더명을 가진 작업분에 대해 실패")
+            print(error)
+            traceback.print_exc()
+
+    return 결과객체모음
+
+def 카운팅(config, 결과객체모음):
     
+    print("카운팅 시작")
+
+    try:
+
+        카운팅_업데이트(config, 결과객체모음, 프린트여부=True, 파일업데이트여부=True)
+
+    except Exception as e:
+
+        print(f"카운팅 과정에서 오류: {e}")
+        traceback.print_exc()
+
+def logging(결과객체모음):
+    
+    print("덤프 시작")
+    
+    try:
+        today = date.today()
+        filename = f"log_{today.strftime('%Y%m%d')}.json"
+
+        with open(filename, "w", encoding="utf-8") as json_file:
+            json.dump(결과객체모음, json_file, ensure_ascii=False)
+
+    except Exception as e:
+        print(f"덤프 과정에서 오류: {e}")
+        traceback.print_exc()
+
+DEFAULT_CONFIG_FILE_PATH = "./apartments.json"
+
+'''
+python3 update_in_folders.py <folder_path> <base_path>
+
+- folder_path: 업데이트할 타겟은 사진들이 폴더별로 정리되어있는 루트 주소
+- base_path: 업데이트될 엑셀 파일의 디렉토리 주소
+
+'''
+if __name__ == '__main__':
+
+    config = load_json(DEFAULT_CONFIG_FILE_PATH)
+
+    folder_path = sys.argv[1]
+    base_path = "." if len(sys.argv) < 3 else sys.argv[2]
+
+    pass_files, folder_entries = get_entries(folder_path)
+    
+    결과객체모음 = update_all_apartment(config, base_path, folder_entries)
+    카운팅(config, 결과객체모음)
+    logging(결과객체모음)
+
+    print("전체 프로세스 종료")
